d88dae2929fe5eb4c923af070a6e0f781678b576
Vishnu Kumar <u48727@ust-global.com>
2018-12-10 06:01:18 -0400

    Initial commit

---
+++ shellTest.sh
@@ -0,0 +1,17 @@
+. shellTest.cfg
+
+echo "whse batch started"
+#select rows from database with the given condition by establishing a connection with the db using userid and password
+echo "SELECT item_nbr, movement_status, storage_location FROM Received_Items where movement_status = 'pending'" | mysql $database -u $userid -p $password > $filename
+
+if [ -f $filename ]
+then
+#setting host of ftp
+echo "initiating file transfer"
+scp $filename $USER@$HOST:/downloads/
+
+else
+        echo "no files to transfer"
+fi
+
+echo "whse batch completed"
